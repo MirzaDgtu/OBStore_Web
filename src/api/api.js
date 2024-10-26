d24f42e2528ca3f3442f40b906e561901c3b5de8
MirzaDgtu <mirzyali@mail.ru>
2024-10-26 23:01:17 +0700

    nht cmm

--- src/api/api.js
+++ src/api/api.js
@@ -14,7 +14,7 @@ api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('Auth');
     if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`;
+      config.headers['Cookie'] = `Bearer ${token}`;
     }
     return config;
   },
